e51d91a01137986b0dc06ddee01c55b56fa54d99
Gabriel Dominguez <gabriel@gabrieldominguez.es>
2023-10-10 19:42:49 +0200

    Improve Main_AC with menu

--- src/main/Main_AC.ts
+++ src/main/Main_AC.ts
@@ -5,9 +5,24 @@ const { BigNumber } = require('ethers');
 const { ethers } = require('hardhat');
 const Web3 = require('web3');
 import {testAccounts} from "../hardhat.config";
+const readLineSync = require('readline-sync')
 
 //Referencias: 
 //https://docs.openzeppelin.com/learn/deploying-and-interacting
+
+
+const listInstitution = async(sc: any) => {
+  var userRes = readLineSync.question("Pick the index: ");
+
+  console.log(await sc.getInstitution(userRes));
+}
+
+const addInstitution = async(sc: any) => {
+  var userRes = readLineSync.question("Pick the public address of the institution: ");
+
+  await sc.addAcademicInstitution(userRes);
+}
+
 
 
 const main = async(): Promise<any> => {
@@ -24,20 +39,41 @@ const main = async(): Promise<any> => {
     console.log("Citizen1 Pub key: " + citizen1.address )
     console.log("Citizen1 Priv key: " + citizen1.privateKey )*/
 
-
     // Load hardhat.config.ts addresses
     const [admin, citizen1] = await ethers.getSigners();
+
 
     // Load contract already deployed in the subnet
     const address = '0xa1E47689f396fED7d18D797d9D31D727d2c0d483'; // Deployed contract address
     var stFactory = await ethers.getContractFactory('AccessControl'); // Interface
     stFactory = stFactory.connect(admin); // change the user who sign the transactionn
     const sc = await stFactory.attach(address);
+
+    let userRes;
+    while (userRes !== '0') {
+        console.log("");
+        console.log("1. List institution")
+        console.log("2. Add institution")
+        userRes = readLineSync.question("Pick an option: ");
+        if (userRes === '1') {
+          await listInstitution(sc);
+        } else if (userRes === '2') {
+          console.log("Admin public address: " + admin.address)
+          console.log("Citizen1 public address: " + citizen1.address)
+          await addInstitution(sc);
+        }
+    }
+
+/*
+
+
+
     
     // Use the contract loaded
     //await sc.addAcademicInstitution(admin.address);
     console.log(await sc.getInstitution(0));
     console.log(await sc.getInstitution(1));
+    */
 }
 
 main()
